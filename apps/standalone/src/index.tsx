a8f123c903e0fc3d45f2bf2debd14613242964fa
Ben Keen <ben.keen@gmail.com>
2022-08-05 20:10:04 -0700

    https update for standalone example

--- apps/standalone/src/index.tsx
+++ apps/standalone/src/index.tsx
@@ -10,7 +10,7 @@ root.render(
 		placeId={config.places[0].placeId}
 		dataSource={DataSource.url}
 		config={config}
-		baseUrl="http://sisyphean.ca/inat"
+		baseUrl="https://sisyphean.ca/inat"
 		itemWidth={180}
 		classes={{
 			tabs: "inat-tabs",
@@ -24,7 +24,7 @@ root.render(
 			tabDesc: "inat-tab-desc"
 		}}
 		tabDescs={{
-			recentDesc: "This dpage lists the most recent butterfly and moth observations made in BC. Click on any of them to go to the inaturalist site for more information.",
+			recentDesc: "This page lists the most recent butterfly and moth observations made in BC. Click on any of them to go to the inaturalist site for more information.",
 			mostCommonDesc: "This page lists the most commonly reported species in BC. You can use the dropdown at the top right to filter the results to a particular year.",
 			mostFavouritedDesc: "Any time iNaturalist users encounter an observation they like, they can choose to \"favourite\" it. This page lists the most favourited moth and butterfly observations made in BC."
 		}}
