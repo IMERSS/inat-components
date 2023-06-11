9732434ffcfef601dd2ca1016d9bef419a58c70b
Ben Keen <ben.keen@gmail.com>
2023-06-11 16:52:30 -0700

    more code shuffling

---
+++ DEVELOPER.md
@@ -0,0 +1,13 @@
+## Developer notes
+
+First, let's get it all going.
+
+1. Clone the repo
+2. In the root folder, run `nvm install` to ensure you're running the compatible version of node (see NVM for more info on that command).
+3. run `yarn install` 
+4. run `yarn boot`
+5. run `yarn build`
+
+Other root monorepo commands:
+
+- `yarn standalone` - starts up a watcher for the standalone script and loads `http://localhost:3000` in your browser to play around with it.
--- packages/inat-components-standalone/rollup.config.js
+++ packages/inat-components-standalone/rollup.config.js
@@ -1,8 +1,9 @@
+import path from 'path';
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonJS from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
-import json from "@rollup/plugin-json";
+// import json from "@rollup/plugin-json";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 
@@ -11,8 +12,11 @@ export default [
         input: "src/index.tsx",
         output: {
             file: "dist/standalone.min.js",
-            format: "es",
-            compact: true // does make it a bit smaller...
+            format: "iife",
+            compact: true, // does make it a bit smaller...
+            paths: {
+                'react/jsx-runtime': path.resolve('./node_modules/react/jsx-runtime.js'),
+            },
         },
         plugins: [
             // these two plugins ensure all dependencies are also included as part of the single generated standalone.min.js file
@@ -26,6 +30,7 @@ export default [
                     // 'node_modules/isomorphic-unfetch'
                 ]
             }),
+
             // this ensures react (and maybe other libs) get their smaller prod bundles included + not the dev code
             replace({
                 'process.env.NODE_ENV': JSON.stringify('production')
@@ -35,8 +40,8 @@ export default [
                 plugins: []
             }),
             typescript(),
-            json(),
-            terser()
+            // json(),
+            // terser()
             // visualizer()
         ]
     }
